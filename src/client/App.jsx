9e32e71089242f76667bfc0ce272bdac5c17d263
Philipp Beau <PhilippB3au@gmail.com>
2017-06-05 20:26:46 +0200

    f

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -5,18 +5,21 @@ export default class Main extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      robots: []
+    };
   }
 
   componentWillMount = () => {
     fetch('/api/robots')
     .then((data) => data.json())
     .then((json) => {
-      
+      this.setState({ robots: json });
     });
   }
 
   render() {
+    const { robots } = this.state;
 
     return (
       <div>Main Container</div>
