ffc23f0626343b9132248fc3a63bccdf0fcd5acf
Philipp Beau <PhilippB3au@gmail.com>
2017-06-05 22:23:32 +0200

    final

--- src/server.js
+++ src/server.js
@@ -7,16 +7,18 @@ import bodyParser from 'body-parser';
 
 /* Get our "DB" */
 const ROBOTS = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../robots.json')).toString());
+const HTML = fs.readFileSync(path.resolve(__dirname, '../index.html')).toString();
 
 const app = express();
 
-app.use('/', express.static(path.resolve(__dirname, '..', 'static')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(`/api/robots`, (req, res) => {
   res.json(ROBOTS);
 });
+
+app.use('*', (req, res) => res.send(HTML));
 
 app.listen(3000, () => {
   console.info('HTTP is running on PORT 3000/');
